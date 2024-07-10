8c53f2a1a971a462569ebec79f937f89efd58f48
Diego <146681634+d13g0x23@users.noreply.github.com>
2024-07-10 15:31:05 -0400

    Se organizo de manera clara. En la clase comunidad se creo el atributo ciudadanos. En la clase enfermedad se creo el atributo para el nombre de la enfermedad :v tipo None, también se modifico el atributo tasarecuperacion para que sea tipo None

--- comunidad.py
+++ comunidad.py
@@ -1,10 +1,19 @@
 class Comunidad:
     def __init__(self, num_ciudadanos, promedio_conexion_fisica, enfermedad, num_infectados, probabilidad_conexion_fisica):
+        """
+            num_ciudadanos = Población de ciudadanos
+            promedio_conexion_fisica = Personas que ve al día
+            enfermedad = Nombre de la enfermedad :v
+            num_infectados = Población de infectados iniciales
+            probabilidad_conexion_fisica = Probabiidad de que se junte con más personas
+            ciudadanos = Lista vacia que recibira a objeto ciudadano
+        """
         self.__num_ciudadanos = num_ciudadanos 
-        self.__promedio_conexion_fisica = promedio_conexion_fisica
-        self.__enfermedad = enfermedad
-        self.__num_infectados = num_infectados
-        self.__probabilidad_conexion_fisica = probabilidad_conexion_fisica
+        self.__promedio_conexion_fisica = promedio_conexion_fisica 
+        self.__enfermedad = enfermedad 
+        self.__num_infectados = num_infectados 
+        self.__probabilidad_conexion_fisica = probabilidad_conexion_fisica 
+        self.__ciudadanos = [] 
 
     # gets y sets de los atributos
     def set_num_ciudadanos(self, num_ciudadanos):
@@ -36,3 +45,9 @@ class Comunidad:
 
     def get_probabilidad_conexion_fisica(self):
         return self.__probabilidad_conexion_fisica
+    
+    def set_ciudadanos(self, ciudadanos):
+        self.__ciudadanos = ciudadanos
+
+    def get_ciudadanos(self):
+        return self.__ciudadanos
--- enfermedad.py
+++ enfermedad.py
@@ -1,9 +1,17 @@
 class Enfermedad:
-    def __init__(self, infeccion_probable, promedio_pasos, infeccion_estrecho, tasarecuperacion):
-        self.__infeccion_probable = infeccion_probable # Probaibilidad de infeccion de una persona (B)
-        self.__promedio_pasos = promedio_pasos # Días con la infección
-        self.__infeccion_estrecho = infeccion_estrecho # Probabilidad de infeccion a un contacto estrecho
-        self.__tasarecuperacion = tasarecuperacion # Tasa en que un ciudadano se recupera de la enfermedad
+    def __init__(self, infeccion_probable, promedio_pasos, infeccion_estrecho):
+        """
+            infeccion_probable = Probaibilidad de infeccion de una persona (B)
+            promedio_pasos = Días con la infección
+            infeccion_estrecho = Probabilidad de infeccion a un contacto estrecho
+            tasarecuperacion = Tasa en que un ciudadano se recupera de la enfermedad
+            nombre_enfermedad = Nombre de la enfermedad :v
+        """
+        self.__infeccion_probable = infeccion_probable 
+        self.__promedio_pasos = promedio_pasos 
+        self.__infeccion_estrecho = infeccion_estrecho 
+        self.__tasarecuperacion = None 
+        self.__nombre_enfermedad = None
     
     # gets y sets de los atributos
     def set_infeccion_probable(self, infeccion_probable):
@@ -29,3 +37,9 @@ class Enfermedad:
 
     def get_tasarecuperacion(self):
         return self.__tasarecuperacion
+    
+    def set_nombre_enfermedad(self, nombre_enfermedad):
+        self.__nombre_enfermedad = nombre_enfermedad
+
+    def get_nombre_enfermedad(self):
+        return self.__nombre_enfermedad
