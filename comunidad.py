c3b130a863620136b7f96a53e9bf02c9e9f6f9da
Diego <146681634+d13g0x23@users.noreply.github.com>
2024-07-11 11:56:23 -0400

    Se elimino el atributo ciudadanos y en su reemplazo se crearon los atributos susceptibles, infectados y recuperados

--- comunidad.py
+++ comunidad.py
@@ -1,3 +1,4 @@
+from enfermedad import Enfermedad
 class Comunidad:
     def __init__(self, num_ciudadanos, promedio_conexion_fisica, enfermedad, num_infectados, probabilidad_conexion_fisica):
         """
@@ -8,12 +9,15 @@ class Comunidad:
             probabilidad_conexion_fisica = Probabiidad de que se junte con más personas
             ciudadanos = Lista vacia que recibira a objeto ciudadano
         """
-        self.__num_ciudadanos = num_ciudadanos 
+        self.__num_ciudadanos = num_ciudadanos # N (Población modelo SIR)
         self.__promedio_conexion_fisica = promedio_conexion_fisica 
         self.__enfermedad = enfermedad 
         self.__num_infectados = num_infectados 
         self.__probabilidad_conexion_fisica = probabilidad_conexion_fisica 
-        self.__ciudadanos = [] 
+        self.__susceptibles = []
+        self.__infectados = []
+        self.__recuperados = []
+
 
     # gets y sets de los atributos
     def set_num_ciudadanos(self, num_ciudadanos):
@@ -46,8 +50,20 @@ class Comunidad:
     def get_probabilidad_conexion_fisica(self):
         return self.__probabilidad_conexion_fisica
     
-    def set_ciudadanos(self, ciudadanos):
-        self.__ciudadanos = ciudadanos
+    def set_susceptibles(self, susceptibles):
+        self.__susceptibles = susceptibles
 
-    def get_ciudadanos(self):
-        return self.__ciudadanos
+    def get_susceptibles(self):
+        return self.__susceptibles
+    
+    def set_infectados(self, infectados):
+        self.__infectados = infectados
+
+    def get_infectados(self):
+        return self.__infectados
+    
+    def set_recuperados(self, recuperados):
+        self.__recuperados = recuperados
+
+    def get_recuperados(self):
+        return self.__recuperados
