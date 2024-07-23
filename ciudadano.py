b5f90ff798e89344106c676f4c768d8ffbe749fe
Diego <146681634+d13g0x23@users.noreply.github.com>
2024-07-23 14:26:21 -0400

    Se elimino lo relacionado al atributo self.__estado y también los métodos iniciar_contagio, duracion_contagio, actualizar_estado, infectar y rescatar_ciudadano. El atributo enfermedad se inicializa como None

--- ciudadano.py
+++ ciudadano.py
@@ -1,100 +1,46 @@
 class Ciudadano:
-    def __init__(self, comunidad, _id, nombre, apellido, familia, enfermedad):
-        """S: suceptible
-           I: infectado
-           R: recuperado"""
+    def __init__(self, comunidad, _id, nombre,
+                 apellido, familia, enfermedad=None):
         self.__comunidad = comunidad
         self.__id = _id
         self.__nombre = nombre
         self.__apellido = apellido
         self.__familia = familia
         self.__enfermedad = enfermedad
-        self.__estado = "S"
-        self.__contador = None
-
 
     # gets y sets de los atributos
     def set_comunidad(self, comunidad):
         self.__comunidad = comunidad
-    
+
     def get_comunidad(self):
         return self.__comunidad
-    
+
     def set_id(self, _id):
         self.__id = _id
 
     def get_id(self):
         return self.__id
-    
+
     def set_nombre(self, nombre):
         self.__nombre = nombre
 
     def get_nombre(self):
         return self.__nombre
-    
+
     def set_apellido(self, apellido):
         self.__apellido = apellido
 
     def get_apellido(self):
         return self.__apellido
-    
+
     def set_familia(self, familia):
         self.__familia = familia
 
     def get_familia(self):
         return self.__familia
-    
+
     def set_enfermedad(self, enfermedad):
         self.__enfermedad = enfermedad
 
     def get_enfermedad(self):
         return self.__enfermedad
-    
-    def set_estado(self, estado):
-        self.__estado = estado
-
-    def get_estado(self):
-        return self.__estado
-    
-    def set_contador(self, contador):
-        self.__contador = contador
-
-    # Métodos para la clase ciudadano
-    def iniciar_contagio(self):
-        """
-        Inicializa el contador de la duración del contagio con promedio_pasos de la enfermedad
-        """
-        self.__contador = self.__enfermedad.get_promedio_pasos() # Son los días que esta contagiado
-
-    def duracion_contagio(self):    
-        """
-        Simula la duración del contagio, en este caso, un contador que decrementa en 1 cada día.    
-        """
-        if self.__contador is not None:
-            self.__contador -= 1
-
-    def actualizar_estado(self):
-        """
-        Actualiza el estado del ciudadano
-        """
-        if self.__estado == "I" and self.__contador is not None:
-            self.duracion_contagio()
-            if self.__contador <= 0:
-                self.__estado = "R"
-                self.__contador = None
-    
-    def infectar(self):
-        """
-        Cambia el estado del ciudadano de "S" a "I" y inicializa el contagio
-        """
-        if self.__estado == "S":
-            self.__estado = "I"
-            self.iniciar_contagio()
-
-    def rescatar_ciudadano(self):
-        """
-        Cambia el estado del ciudadano de "I" a "R" y reinicia el contador
-        """
-        if self.__estado == "I":
-            self.__estado = "R"
-            self.__contador = None
