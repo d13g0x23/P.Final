f30aba3d9e72119967f40b2bfa2f55c904b3b87d
Diego <146681634+d13g0x23@users.noreply.github.com>
2024-07-23 14:49:41 -0400

    Se elimino el atributo infeccion_estrecho junto a sus get y set correspondientes, agregando los atributos enfermo y contador. Se creo un método avanzar_dias

--- enfermedad.py
+++ enfermedad.py
@@ -1,5 +1,5 @@
 class Enfermedad:
-    def __init__(self, infeccion_probable, promedio_pasos, infeccion_estrecho):
+    def __init__(self, infeccion_probable, promedio_pasos, enfermo=False):
         """
             infeccion_probable = Probaibilidad de infeccion de una persona (B)
             promedio_pasos = Son los DÍAS que esta CONTAGIADO o con la infección
@@ -7,25 +7,21 @@ class Enfermedad:
         """
         self.__infeccion_probable = infeccion_probable # B (tasa de transmision modelo SIR)
         self.__promedio_pasos = promedio_pasos # Días que esta contagiado / Tasa de recuperación
-        self.__infeccion_estrecho = infeccion_estrecho 
+        self.__enfermo = enfermo
+        self.__contador = promedio_pasos  # Empieza a contar hacia abajo
 
-    
     # gets y sets de los atributos
-    def set_infeccion_probable(self, infeccion_probable):
-        self.__infeccion_probable = infeccion_probable
-    
     def get_infeccion_probable(self):
         return self.__infeccion_probable
-    
-    def set_promedio_pasos(self, promedio_pasos):
-        self.__promedio_pasos = promedio_pasos
+
+    def get_enfermo(self):
+        return self.__enfermo
 
     def get_promedio_pasos(self):
         return self.__promedio_pasos
-    
-    def set_infeccion_estrecho(self, infeccion_estrecho):
-        self.__infeccion_estrecho = infeccion_estrecho
 
-    def get_infeccion_estrecho(self):
-        return self.__infeccion_estrecho
-       
+    # Método para avanzar la infección y curar, si corresponde
+    def avanzar_dias(self):
+        self.__contador -= 1
+        if self.__contador == 0:
+            self.__enfermo = False
